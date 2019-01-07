180f79b09f1acb2487e5b60fc47b9557ada2c3ab
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-01-07 09:15:34 -0500

    CLI is now working properly

--- src/HistoReg.cxx
+++ src/HistoReg.cxx
@@ -2,12 +2,12 @@
 
 int main(int argc, char **argv)
 {
-  if (argc < 3)
+  if (argc < 5)
   {
     std::cerr << "HistoReg Usage:\n  HistoReg.exe -d $pathToDataDir -o $pathToOutputDir \n";
     return EXIT_FAILURE;
   }
-  std::string dataDir = argv[1];
-  std::string outputDir = argv[2];
+  std::string dataDir = argv[2];
+  std::string outputDir = argv[4];
   return EXIT_SUCCESS;
 }
