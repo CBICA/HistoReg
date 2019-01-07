1c05df6f2b78eaf12930965fc42572be342ccaf6
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-01-07 09:18:47 -0500

    making the usage a bit easier to read

--- src/HistoReg.cxx
+++ src/HistoReg.cxx
@@ -2,12 +2,18 @@
 
 int main(int argc, char **argv)
 {
-  if (argc < 5)
+  if (argc != 5)
   {
-    std::cerr << "HistoReg Usage:\n  HistoReg.exe -d $pathToDataDir -o $pathToOutputDir \n";
+    std::cerr << "HistoReg Usage:\n  HistoReg.exe -d C:/path/to/dataDir -o C:/path/to/outputDir \n";
     return EXIT_FAILURE;
   }
-  std::string dataDir = argv[2];
-  std::string outputDir = argv[4];
+  const std::string dataDir = argv[2];
+  const std::string outputDir = argv[4];
+
+  if (!cbica::isDir(outputDir))
+  {
+    cbica::createDir(outputDir);
+  }
+
   return EXIT_SUCCESS;
 }
