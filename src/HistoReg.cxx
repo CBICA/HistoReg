1dff2f8b3d231cc6ccd881cfcc39cebfd22ae34f
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-01-07 09:21:54 -0500

    added comments and started making the base structure

--- src/HistoReg.cxx
+++ src/HistoReg.cxx
@@ -15,5 +15,29 @@ int main(int argc, char **argv)
     cbica::createDir(outputDir);
   }
 
+  // get all the sub-directories in the folder
+  auto foldersInDir = cbica::subdirectoriesInDirectory(dataDir);
+  
+  for (size_t i = 0; i < foldersInDir.size(); i++)
+  {
+    // get all the files in the sub-directory
+    auto filesInSubDir = cbica::filesInDirectory(foldersInDir[i]);
+
+    // process each file in the sud-directory
+    for (size_t j = 0; j < filesInSubDir.size(); j++)
+    {
+      std::string path, base, ext;
+      cbica::splitFileName(filesInSubDir[j], path, base, ext);
+      if (ext == ".png")
+      {
+        // do something with the image
+      }
+      else if (ext == ".csv")
+      {
+        // do something with the landmark
+      }
+    }
+  }
+
   return EXIT_SUCCESS;
 }
