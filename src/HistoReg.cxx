5e50ee71bda16d51e8211d0117e6f4590d437fdb
Sarthak Pati <sarthak.pati@uphs.upenn.edu>
2019-01-09 14:50:57 -0500

    getting the translation vector from the robust matches properly

--- src/HistoReg.cxx
+++ src/HistoReg.cxx
@@ -3,6 +3,19 @@
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/xfeatures2d.hpp"
+//#include "opencv2/optflow.hpp"
+
+//! A helper struct to store some variables
+struct TranslationUnit
+{
+  float x; //! translation along x
+  float y; //! translation along y
+  float absoluteDistance; //! absolute distance between x and y - does NOT account for direction
+  void Update()
+  {
+    absoluteDistance = std::sqrtf(std::pow(x, 2) + std::pow(y, 2));
+  }
+};
 
 int main(int argc, char **argv)
 {
@@ -31,14 +44,14 @@ int main(int argc, char **argv)
   //auto f2d = cv::xfeatures2d::DAISY::create();
 
   // Detect the keypoints:
-  std::vector< cv::KeyPoint > keypoints_1, keypoints_2;
-  f2d->detect(sourceImage, keypoints_1);
-  f2d->detect(targetImage, keypoints_2);
+  std::vector< cv::KeyPoint > keypoints_source, keypoints_target;
+  f2d->detect(sourceImage, keypoints_source);
+  f2d->detect(targetImage, keypoints_target);
 
   // Calculate descriptors (feature vectors)    
   cv::Mat descriptors_1, descriptors_2;
-  f2d->compute(sourceImage, keypoints_1, descriptors_1);
-  f2d->compute(targetImage, keypoints_2, descriptors_2);
+  f2d->compute(sourceImage, keypoints_source, descriptors_1);
+  f2d->compute(targetImage, keypoints_target, descriptors_2);
 
   // Matching descriptor vectors
   std::vector< cv::DMatch > matches;
@@ -59,16 +72,38 @@ int main(int argc, char **argv)
   //    max_dist = dist;
   //}
 
-  /// https://docs.opencv.org/3.4.3/d9/d0c/group__calib3d.html#ga4abc2ece9fab9398f2e560d53c8c9780
-  auto h = cv::findHomography(keypoints_1, keypoints_2, cv::RANSAC); // change method of homography to 0, LMEDS or RHO (documentation in above link)
-  cv::imwrite(outputDir + "/homography.jpg", h);
+  auto robust_matches = matches;
+  robust_matches.clear();
+  for (size_t i = 0; i < matches.size(); i++)
+  {
+    if (std::abs(matches[i].distance) < 0.6) // lowe's ratio to be changed
+    {
+      robust_matches.push_back(matches[i]);
+    }
+  }
 
-  auto sourceImage_reg = sourceImage;
+  /// accessing points in robust_matches and making that into a translation vector
+  std::vector< TranslationUnit > translationVector;
+  for (size_t i = 0; i < robust_matches.size(); i++)
+  {
+    TranslationUnit temp;
+    temp.x = keypoints_target[robust_matches[i].imgIdx].pt.x - keypoints_source[robust_matches[i].imgIdx].pt.x;
+    temp.y = keypoints_target[robust_matches[i].imgIdx].pt.y - keypoints_source[robust_matches[i].imgIdx].pt.y;
+    temp.Update();
+    translationVector.push_back(temp);
+  }
 
-  /// https://docs.opencv.org/3.4.3/da/d54/group__imgproc__transform.html#gaf73673a7e8e18ec6963e3774e6a94b87
-  cv::warpPerspective(sourceImage, sourceImage_reg, h, targetImage.size());
+  //robust_points_source[0].pt.x
+  ///// https://docs.opencv.org/3.4.3/d9/d0c/group__calib3d.html#ga4abc2ece9fab9398f2e560d53c8c9780
+  //auto h = cv::findHomography(keypoints_source, keypoints_target, cv::RANSAC,); // change method of homography to 0, LMEDS or RHO (documentation in above link)
+  //cv::imwrite(outputDir + "/homography.jpg", h);
 
-  cv::imwrite(outputDir + "/sourceImage_reg.jpg", sourceImage_reg);
+  //auto sourceImage_reg = sourceImage;
+
+  ///// https://docs.opencv.org/3.4.3/da/d54/group__imgproc__transform.html#gaf73673a7e8e18ec6963e3774e6a94b87
+  //cv::warpPerspective(sourceImage, sourceImage_reg, h, targetImage.size());
+
+  //cv::imwrite(outputDir + "/sourceImage_reg.jpg", sourceImage_reg);
   //if (argc != 5)
   //{
   //  std::cerr << "HistoReg Usage:\n  HistoReg.exe -d C:/path/to/dataDir -o C:/path/to/outputDir \n";
